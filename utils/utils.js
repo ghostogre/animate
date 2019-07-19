04a2faf6dd74b637e99304627243888d41706eaf
yingjh <yjh.1234@qq.com>
2019-07-19 17:54:17 +0800

    粒子动画

--- utils/utils.js
+++ utils/utils.js
@@ -67,5 +67,21 @@ window.utils = {
       touch.event = event;
     }, false);
     return touch;
+  },
+  parseColor: function (color, toNumber) {
+    if (toNumber === true) {
+      if (typeof color === 'number') {
+        return (color | 0); //chop off decimal
+      }
+      if (typeof color === 'string' && color[0] === '#') {
+        color = color.slice(1);
+      }
+      return window.parseInt(color, 16);
+    } else {
+      if (typeof color === 'number') {
+        color = '#' + ('00000' + (color | 0).toString(16)).substr(-6); //pad
+      }
+      return color;
+    }
   }
 };
